4253f62952b32afe74ff135f85efe99edb98d709
kanazahid929 <puresingleking@gmail.com>
2025-11-22 18:45:06 +0600

    Update npx5-romantic.js

--- scripts/cmds/npx5-romantic.js
+++ scripts/cmds/npx5-romantic.js
@@ -26,7 +26,7 @@ module.exports = {
 
 		if (triggers.some(t => text.includes(t))) {
 
-			const filePath = __dirname + "/siyam/rm.mp3";
+			const filePath = __dirname + "/siyam/bold.mp3";
 
 			api.sendMessage({
 				body: "😻🍭",
