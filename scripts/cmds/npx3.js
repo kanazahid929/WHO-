d79872fc4fc3d8af5942b07e9d407f735e9d019a
kanazahid929 <puresingleking@gmail.com>
2025-11-22 17:33:04 +0600

    Create npx3.js

---
+++ scripts/cmds/npx3.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+module.exports = {
+	config: {
+		name: "npx1",
+		version: "1.0",
+		author: "Mesbah Saxx",
+		countDown: 5,
+		role: 0,
+		description: {
+			en: "Auto play audio when trigger words are detected"
+		},
+		category: "no prefix",
+		guide: {
+			en: "no prefix needed - auto trigger"
+		}
+	},
+
+	onChat: async function ({ api, event }) {
+		const { threadID, messageID, body } = event;
+		if (!body) return;
+
+		const text = body.toLowerCase();
+
+		const triggers = ["😌", "npx", "love", "valo", "hi"];
+
+		if (triggers.some(t => text.includes(t))) {
+
+			const filePath = __dirname + "/siyam/fg.mp3";
+
+			api.sendMessage({
+				body: "❤️‍🔥😺",
+				attachment: fs.createReadStream(filePath)
+			}, threadID, messageID);
+
+			api.setMessageReaction("💋", messageID, () => {}, true);
+		}
+	},
+
+	onStart: async function () {}
+};
