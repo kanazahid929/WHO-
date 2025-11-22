784f3fd75035a9377b7b2d6e21861be8362145b2
kanazahid929 <puresingleking@gmail.com>
2025-11-22 18:31:46 +0600

    Create npx5-romantic.js

---
+++ scripts/cmds/npx5-romantic.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+module.exports = {
+	config: {
+		name: "npx3",
+		version: "1.0",
+		author: "siyam-1",
+		countDown: 5,
+		role: 0,
+		description: {
+			en: "Auto play audio when trigger words are detected"
+		},
+		category: "no prefix",
+		guide: {
+			en: "no prefix needed - auto trigger"
+		}
+	},
+
+	onChat: async function ({ api, event }) {
+		const { threadID, messageID, body } = event;
+		if (!body) return;
+
+		const text = body.toLowerCase();
+
+		const triggers = ["😍", "☺️", "😘", "😽", "🫣"];
+
+		if (triggers.some(t => text.includes(t))) {
+
+			const filePath = __dirname + "/siyam/siyam1.mp3";
+
+			api.sendMessage({
+				body: "😻🍭𝐂𝐄𝐎⸙𝐒𝐄𝐘𝐀𝐌𓆪🍥🧸",
+				attachment: fs.createReadStream(filePath)
+			}, threadID, messageID);
+
+			api.setMessageReaction("👀", messageID, () => {}, true);
+		}
+	},
+
+	onStart: async function () {}
+};
